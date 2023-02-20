18f68494a0e829ae4b274714e3d59e8fd1c4f385
mukabyl <mukabyl@yamdex.ru>
2023-02-20 23:16:12 +0300

    first commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,32 @@
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        ArrayList<City> arrayList = new ArrayList<>();
+        Random ran = new Random();
+        arrayList.add(new City(ran.nextInt(10), "Moscow"));
+        arrayList.add(new City(ran.nextInt(10), "Bishkek"));
+        arrayList.add(new City(ran.nextInt(10), "Osh"));
+        arrayList.add(new City(ran.nextInt(10), "Batken"));
+        arrayList.add(new City(ran.nextInt(10), "Naryn"));
+        arrayList.add(new City(ran.nextInt(10), "Talas"));
+        arrayList.add(new City(ran.nextInt(10), "Yssyk-kol"));
+        arrayList.add(new City(ran.nextInt(10), "Jalal-abad"));
+        arrayList.add(new City(ran.nextInt(10), "Chui"));
+        arrayList.add(new City(ran.nextInt(10), "New York"));
+
+        for (City city : arrayList) {
+            System.out.println(city);
+        }
+        System.out.println();
+
+        TreeSet<City> treeCity = new TreeSet<>(City.cityGetCode);
+        for (City city1 : arrayList) {
+            if (city1.getCode() % 2 != 0){
+                treeCity.add(city1);
+            }
+        }
+        treeCity.forEach(System.out::println);
+
     }
 }
